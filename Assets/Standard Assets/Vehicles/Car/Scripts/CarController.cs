84da464efe7c33601c6bad00278713022c765e70
h23i03 <h23i06@yahoo.co.jp>
2015-07-17 16:04:31 +0900

    車の動きを録画、再生できるようにした

--- Assets/Standard Assets/Vehicles/Car/Scripts/CarController.cs
+++ Assets/Standard Assets/Vehicles/Car/Scripts/CarController.cs
@@ -1,6 +1,53 @@
 using System;
 using UnityEngine;
 
+class WriteTextFile
+{
+	static void Main()
+	{
+		
+		// These examples assume a "C:\Users\Public\TestFolder" folder on your machine.
+		// You can modify the path if necessary. 
+		
+		// Example #1: Write an array of strings to a file. 
+		// Create a string array that consists of three lines. 
+		string[] lines = { "First line", "Second line", "Third line" };
+		// WriteAllLines creates a file, writes a collection of strings to the file, 
+		// and then closes the file.
+		System.IO.File.WriteAllLines(@"C:\Users\h23i03\Desktop\test.txt", lines);
+		
+		
+		// Example #2: Write one string to a text file. 
+		string text = "A class is the most powerful data type in C#. Like a structure, " +
+			"a class defines the data and behavior of the data type. ";
+		// WriteAllText creates a file, writes the specified string to the file, 
+		// and then closes the file.
+		System.IO.File.WriteAllText(@"C:\Users\h23i03\Desktop\WriteText.txt", text);
+		
+		// Example #3: Write only some strings in an array to a file. 
+		// The using statement automatically closes the stream and calls  
+		// IDisposable.Dispose on the stream object. 
+		using (System.IO.StreamWriter file = new System.IO.StreamWriter(@"C:\Users\h23i03\Desktop\test.txt"))
+		{
+			foreach (string line in lines)
+			{
+				// If the line doesn't contain the word 'Second', write the line to the file. 
+				if (!line.Contains("Second"))
+				{
+					file.WriteLine(line);
+				}
+			}
+		}
+		
+		// Example #4: Append new text to an existing file. 
+		// The using statement automatically closes the stream and calls  
+		// IDisposable.Dispose on the stream object. 
+		using (System.IO.StreamWriter file = new System.IO.StreamWriter(@"C:\Users\h23i03\Desktop\test.txt", true))
+		{
+			file.WriteLine("Fourth line");
+		}
+	}
+}
 namespace UnityStandardAssets.Vehicles.Car
 {
     internal enum CarDriveType
@@ -128,6 +175,7 @@ namespace UnityStandardAssets.Vehicles.Car
 
         public void Move(float steering, float accel, float footbrake, float handbrake)
         {
+			//accel = 10000;
             for (int i = 0; i < 4; i++)
             {
                 Quaternion quat;
@@ -138,9 +186,9 @@ namespace UnityStandardAssets.Vehicles.Car
             }
 
             //clamp input values
-            steering = Mathf.Clamp(steering, -1, 1);
-            AccelInput = accel = Mathf.Clamp(100000, 0, 1);
-            BrakeInput = footbrake = -1*Mathf.Clamp(footbrake, -1, 0);
+			steering = Mathf.Clamp(steering, -1, 1);
+			AccelInput = accel = Mathf.Clamp(accel, 0, 1);
+			BrakeInput = footbrake = -1*Mathf.Clamp(footbrake, -1, 0);
             handbrake = Mathf.Clamp(handbrake, 0, 1);
 
             //Set the steer on the front wheels.
