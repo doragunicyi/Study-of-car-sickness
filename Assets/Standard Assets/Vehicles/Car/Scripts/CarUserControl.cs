ff0dca912826703f6f3e0c4ed151768f6ed98674
h23i03 <h23i06@yahoo.co.jp>
2015-07-17 17:01:15 +0900

    軽くリファクタリング

--- Assets/Standard Assets/Vehicles/Car/Scripts/CarUserControl.cs
+++ Assets/Standard Assets/Vehicles/Car/Scripts/CarUserControl.cs
@@ -12,7 +12,7 @@ namespace UnityStandardAssets.Vehicles.Car
         private CarController m_Car; // the car controller we want to use
 		private FileStream logFile;
 
-		private bool isRecordMode = false;//false;
+		private bool isRecordMode = false;
 
 		private readonly string RECORD_FILE = @"C:\Users\h23i03\Desktop\test.ito";
 
@@ -22,58 +22,59 @@ namespace UnityStandardAssets.Vehicles.Car
             m_Car = GetComponent<CarController>();
         }
 
-		private void Update()
-		{
-
-		}
 		private void Start()
 		{
 			if (isRecordMode) {
 				logFile = new FileStream (RECORD_FILE, FileMode.Create, FileAccess.Write);
 			} else {
 				logFile = new FileStream (RECORD_FILE, FileMode.Open, FileAccess.Read);
-				Debug.Log(logFile.Length);
 			}
 		}
+
 		private void OnApplicationQuit()
 		{
 			logFile.Close ();
 		}
+
+		private Vector2 getVelocity(bool up, bool down, bool right, bool left) {
+			var ret = new Vector2 (0f, 0f);
+			if (right)
+				ret.x += 1.0f;
+			if (left)
+				ret.x -= 1.0f;
+			if (up)
+				ret.y += 1.0f;
+			if (down)
+				ret.y -= 1.0f;
+			return ret;
+		}
+
         private void FixedUpdate()
         {
-            // pass the input to the car!
-            //float h = Input.GetAxis("Horizontal");
-            //float v = Input.GetAxis("Vertical");
-			//Debug.LogFormat ("{0} {1}", h, v);
-            //m_Car.Move(h, v, v, 0f);
-
 			if (isRecordMode) {
-				byte up = (byte)(Input.GetKey (KeyCode.UpArrow) ? 1 : 0);
-				byte down = (byte)(Input.GetKey (KeyCode.DownArrow) ? 1 << 1 : 0);
-				byte right = (byte)(Input.GetKey (KeyCode.RightArrow) ? 1 << 2 : 0);
-				byte left = (byte)(Input.GetKey (KeyCode.LeftArrow) ? 1 << 3 : 0);
+				bool up    = Input.GetKey (KeyCode.UpArrow);
+				bool down  = Input.GetKey (KeyCode.DownArrow);
+				bool right = Input.GetKey (KeyCode.RightArrow);
+				bool left  = Input.GetKey (KeyCode.LeftArrow);
 				
-				logFile.WriteByte ((byte)(up | down | right | left));
-				
-				float h = 0f, v = 0f;
-				if(Input.GetKey(KeyCode.RightArrow)) h += 1.0f;
-				if(Input.GetKey(KeyCode.LeftArrow))  h -= 1.0f;
-				if(Input.GetKey(KeyCode.UpArrow))    v += 1.0f;
-				if(Input.GetKey(KeyCode.DownArrow))  v -= 1.0f;
-				m_Car.Move(h, v, v, 0f);
+				logFile.WriteByte ((byte)((up ? 0x1 : 0x0) | 
+				                          (down ? 0x2 : 0x0) | 
+				                          (right ? 0x4 : 0x0) | 
+				                          (left ? 0x8 : 0x0)));
+
+				var v = getVelocity(up, down, right, left);
+				m_Car.Move(v.x, v.y, v.y, 0f);
+
 			} else {
 				int data = logFile.ReadByte();
-				bool up = (data & 0x1) == 0x1,
-				down = (data & 0x2) == 0x2,
-				right = (data & 0x4) == 0x4,
-				left = (data & 0x8) == 0x8;
+
+				bool up    = (data & 0x1) != 0;
+				bool down  = (data & 0x2) != 0;
+				bool right = (data & 0x4) != 0;
+				bool left  = (data & 0x8) != 0;
 				
-				float h = 0f, v = 0f;
-				if(right) h += 1.0f;
-				if(left)  h -= 1.0f;
-				if(up)    v += 1.0f;
-				if(down)  v -= 1.0f;
-				m_Car.Move(h, v, v, 0f);
+				var v = getVelocity(up, down, right, left);
+				m_Car.Move(v.x, v.y, v.y, 0f);
 			}
 
         }
